78ff930012138dc1b8cad1ecbfe636274cc63680
RokasTverijonas <tverijonasr@gmail.com>
2026-06-29 20:11:04 +0300

    Initialize project

---
+++ src/test/java/com/rokas/runtrack/RunTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokas.runtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
